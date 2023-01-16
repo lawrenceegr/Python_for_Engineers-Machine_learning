ddb298a4b8a8e02f6cc1898d52700a612fb53854
lawrenceegr <lawrencelangatmi@gmail.com>
2023-01-16 09:40:10 +0300

    learned tuples

--- 1.Getting Started/1.1data_types/tuples.py
+++ 1.Getting Started/1.1data_types/tuples.py
@@ -1,1 +1,23 @@
 #unlike lists tuples are enclosed in parenthesis and the objects cannot be changed
+
+
+# creating a tuple in python
+Tuple = ()
+print ("Empty Tuple : ", Tuple
+)
+
+# printing tuple with sample data
+Tuple = ('a','b','c','d',1,2,3,4)
+print("Sample Tuple", Tuple)
+
+# Accessing data in a tuple 
+
+print("Third item in a tuple", Tuple[2])
+
+# length of a tuple
+
+print("length Tuple" , len(Tuple))
+
+# deleting a tuple 
+# del Tuple
+# print("Sample Tuple", Tuple)
